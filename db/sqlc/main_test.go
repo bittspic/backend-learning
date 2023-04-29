96466e3ff6260f8e982ba990357ee957ead61751
Hien Nguyen <bittspic@gmail.com>
2023-04-29 22:46:48 +0700

    use viper to load config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -4,21 +4,22 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"simplebank/util"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
-var testDB  *sql.DB
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+	// var err error
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Can not connect to db:", err)
 	}
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
+	github.com/spf13/viper v1.15.0 // indirect
 	github.com/stretchr/testify v1.8.2
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
