e89e7849b30a6c794c72449dd3f5a6771465488a
Hien Nguyen <bittspic@gmail.com>
2023-05-05 22:50:48 +0700

    implement transfer money API with custom params validator

--- util/random.go
+++ util/random.go
@@ -40,7 +40,7 @@ func RandomMoney() int64 {
 
 // generates random currency
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD"}
+	currencies := []string{EUR, USD, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
